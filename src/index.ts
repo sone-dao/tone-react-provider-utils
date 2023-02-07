3674fe335aeac7d2526d24851876a13074bb5c9f
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 15:25:40 -0500

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default } from './UtilitiesContext'
+import UtilitiesProvider, { useUtilitiesContext } from './UtilitiesContext'
+
+export { useUtilitiesContext }
+export default UtilitiesProvider
